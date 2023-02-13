967264fd0b38e273aa747a25da825205c2dfa1c8
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-02-13 16:23:16 -0300

    Add files via upload
    sito re organizado

--- scripts.js
+++ scripts.js
@@ -1,67 +1,42 @@
 console.log('desde js');
 const $d = document;
 const $header = $d.getElementById('header');
-const $fakeHeader = $d.getElementById('fake_header');
-const $navbarBtnOpen = $d.getElementById('header_btn_menu');
-const $navbarBtnClose = $d.getElementById('navbarBtnClose');
-const $navbar = $d.getElementById('header_navbar');
-const $aboutmeShowMore = $d.getElementById('aboutme_showmore');
+const $navbarBtnOpen = $d.getElementById('header__btn-menu-open');
+const $navbarBtnClose = $d.getElementById('header__btn-menu-close');
+const $navbar = $d.getElementById('header__navbar');
 const $topArrow = $d.getElementById('top_arrow');
 
 
 $d.addEventListener('click', (e) => {  
     
     
-    if (e.target.id == 'navbarBtnClose'){
+    if (e.target.id == 'header__btn-menu-close'){
         console.log('click en el boton de cerrar el menu');
         $navbar.classList.toggle('navbar_show');
         enableScroll();
     }
-    if (e.target.id == 'header_btn_menu'){
+    if (e.target.id == 'header__btn-menu-open'){
         console.log('click en el boton de abrir el menu');
         $navbar.classList.toggle('navbar_show')
         disableScroll();
     }
     if (e.target.id.includes('header_navbar_link')){
-        console.log("btn navbar")
+        console.log(e.target.id)
         $navbar.classList.toggle('navbar_show');
-        enableScroll();
-    }
-    if (e.target.id == 'aboutme_showmore' || e.target.id == 'myskills_showmore' || e.target.id == 'recentprojects_showmore' ){                
-                
-        console.log(e.target);        
-        let nameSection = e.target.id.slice(0, -9);
-        let currentMoreClass = `${nameSection}_textcontent_more`;
-        
-        const $currentMore = $d.querySelector(`.${currentMoreClass}`);        
-        console.log($currentMore);        
-        let isOpen = !$currentMore.classList.contains(`${currentMoreClass}_expanded`);    
-        //console.log(isOpen);
-               
-        let textContent = ""; 
-        //console.log(isOpen);   
-        if (isOpen == true){
-            textContent = '<i class="fa-solid fa-angle-up "></i> Show me less <i class="fa-solid fa-angle-up "></i>'                        
-        }else{
-            textContent = '<i class="fa-solid fa-angle-down "></i> Show me more <i class="fa-solid fa-angle-down "></i>'           
-        }        
-        e.target.innerHTML = textContent; 
-        $currentMore.classList.toggle(`${currentMoreClass}_expanded`);     
-           
+        enableScroll();        
     }    
-    //console.log(window.scrollY);
+    if (e.target.id.includes('header_navbar-desktop_link')){
+        $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
+            item.classList.remove('navbar-selected');
+            console.log(item)
+        });            
+
+        e.target.classList.add('navbar-selected');
+    }    
 }
 );
 window.addEventListener('scroll', (e) => {
-    /*
-    if (window.scrollY > 0){
-        $header.classList.add('navbar_fixed');   
-        $fakeHeader.style.height = '70px';
-    }else{
-        $header.classList.remove('navbar_fixed');        
-        $fakeHeader.style.height = '0px';
-    }  
-    */  
+     
     if (window.scrollY > 70){
         $topArrow.classList.add('top_arrow_show');
     }else{
