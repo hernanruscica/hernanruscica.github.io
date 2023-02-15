a937338f45b32f177f76cf03541168e37b7c826e
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-02-15 12:42:58 -0300

    Add files via upload
    Retoques tamaños mobile (botones chicos). imagen de proyecto con zoom

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # hernanruscica.github.io
 Sitio web de portfolio
-<h1>My personal Web Page, fully responsive</h1>
-<a href="https://hernanruscica.github.io/">demo ONLINE</a>
-<a href="https://hernanruscica.github.io/portfolio/heladeria/index.html" target = "_blank">Heladeria Monticelli</a> 
-<p>It includes JS, CSS, and HTML projects.</p>
-
+<a href="https://hernanruscica.github.io/">demo online</a>
---
+++ scripts.js
@@ -0,0 +1,68 @@
+console.log('desde js');
+const $d = document;
+const $header = $d.getElementById('header');
+const $navbarBtnOpen = $d.getElementById('header__btn-menu-open');
+const $navbarBtnClose = $d.getElementById('header__btn-menu-close');
+const $navbar = $d.getElementById('header__navbar');
+const $topArrow = $d.getElementById('top_arrow');
+
+
+$d.addEventListener('click', (e) => {  
+    
+    
+    if (e.target.id == 'header__btn-menu-close'){
+        console.log('click en el boton de cerrar el menu');
+        $navbar.classList.toggle('navbar_show');
+        enableScroll();
+    }
+    if (e.target.id == 'header__btn-menu-open'){
+        console.log('click en el boton de abrir el menu');
+        $navbar.classList.toggle('navbar_show')
+        disableScroll();
+    }
+    if (e.target.id.includes('header_navbar_link')){
+        console.log(e.target.id)
+        $navbar.classList.toggle('navbar_show');
+        enableScroll();        
+    }    
+    if (e.target.id.includes('header_navbar-desktop_link')){
+        $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
+            item.classList.remove('navbar-selected');
+            console.log(item)
+        });            
+
+        e.target.classList.add('navbar-selected');
+    }    
+}
+);
+window.addEventListener('scroll', (e) => {
+     
+    if (window.scrollY > 70){
+        $topArrow.classList.add('top_arrow_show');
+    }else{
+        $topArrow.classList.remove('top_arrow_show');
+    }
+   //console.log("scrolling", window.scrollY);
+}
+)
+/*
+html {scroll-behavior: smooth;}
+*/
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+        //console.log($d.querySelector('html').setAttribute('scroll-behavior', 'unset'));
+        $d.querySelector('html').setAttribute('style', 'scroll-behavior: unset');
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+    //$d.html.setAttribute('scroll-behavior', 'smooth');
+    $d.querySelector('html').setAttribute('style', 'scroll-behavior: smooth');
+}
