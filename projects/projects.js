151288cc74498362b28570e0968bfa7160cb1b69
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-02-16 16:26:23 -0300

    Add files via upload
    algunos retoques en los estilos del index, textos del index y el enlace al CV

--- projects/projects.js
+++ projects/projects.js
@@ -1,10 +1,17 @@
 console.log("desde projects.js");
 
 function zoom(e){
+  //console.log(e.target);
     var zoomer = e.currentTarget;
+    /*
     e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageX
+    */
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
     x = offsetX/zoomer.offsetWidth*100
     y = offsetY/zoomer.offsetHeight*100
     zoomer.style.backgroundPosition = x + '% ' + y + '%';
   }
+
+  
