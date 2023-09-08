1cc3e8bded9ada4765bb6d216762bc028342cd07
hernanruscica <cesarhernanruscica@gmail.com>
2023-09-08 14:02:35 -0300

    marcado de la seccion actual en los navbar mobile y desktop

--- scripts.js
+++ scripts.js
@@ -20,9 +20,9 @@ $d.addEventListener('click', (e) => {
         $navbar.classList.toggle('navbar_show')
         disableScroll();
     }
-    if (e.target.id.includes('header_navbar_link') || e.target.id.includes('header_navbar-desktop_link')){
+    if (e.target.id.includes('mobile_navbar_link') || e.target.id.includes('header_navbar-desktop_link')){
         //Se hizo click en algun enlace del menu desktop o mobile     
-        
+        console.log('click en  id = "mobile_navbar_link"')
         /*sistema de logros*/
         incrementarProgreso('menuClick');
         console.log(progreso['menuClick']);
@@ -31,14 +31,14 @@ $d.addEventListener('click', (e) => {
         $navbar.classList.toggle('navbar_show');
         enableScroll();        
     }    
-    if (e.target.id.includes('header_navbar-desktop_link')){
-        $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
-            item.classList.remove('navbar-selected');
-           // console.log(item)
-        });            
+    // if (e.target.id.includes('header_navbar-desktop_link')){
+    //     $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
+    //         item.classList.remove('navbar-selected');
+    //        // console.log(item)
+    //     });            
 
-        e.target.classList.add('navbar-selected');
-    }    
+    //     e.target.classList.add('navbar-selected');
+    // }    
 }
 );
 window.addEventListener('scroll', (e) => {
@@ -51,9 +51,7 @@ window.addEventListener('scroll', (e) => {
    //console.log("scrolling", window.scrollY);
 }
 )
-/*
-html {scroll-behavior: smooth;}
-*/
+
 function disableScroll() {
     // Get the current page scroll position
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -72,26 +70,4 @@ function enableScroll() {
     //$d.html.setAttribute('scroll-behavior', 'smooth');
     $d.querySelector('html').setAttribute('style', 'scroll-behavior: smooth');
 }
-
-window.addEventListener('scroll', () => {
-    let idSection01 = 'aboutme';
-    let idSection02 = 'myskills';
-    let idSection03 = 'recentprojects';
-    let idSection04 = 'contactme';
-    let element01 = document.getElementById(idSection01); 
-    let element02 = document.getElementById(idSection02); 
-    let element03 = document.getElementById(idSection03); 
-    let element04 = document.getElementById(idSection04); 
-    
-    let ySection01 = window.scrollY - element01.offsetTop ; // Restar el desplazamiento vertical del scroll con la posición del elemento
-    let ySection02 = window.scrollY - element02.offsetTop ;
-    let ySection03 = window.scrollY - element03.offsetTop ;
-    let ySection04 = window.scrollY - element04.offsetTop ;
-    console.clear();
-    console.log(idSection01, ySection01);  
-    console.log(idSection02, ySection02);  
-    console.log(idSection03, ySection03);  
-    console.log(idSection04, ySection04);  
-    
-})
 
---
+++ scroll_sections.js
@@ -0,0 +1,56 @@
+window.addEventListener('scroll', () => {    
+    const $d = document;
+    let idSection01 = 'aboutme';
+    let idSection02 = 'myskills';
+    let idSection03 = 'recentprojects';
+    let idSection04 = 'contactme';    
+
+    let element01 = $d.getElementById(idSection01); 
+    let element02 = $d.getElementById(idSection02); 
+    let element03 = $d.getElementById(idSection03); 
+    let element04 = $d.getElementById(idSection04);  
+
+    let ySection01 =  element01.offsetTop ; 
+    let ySection02 =  element02.offsetTop ;
+    let ySection03 =  element03.offsetTop ;
+    let ySection04 =  element04.offsetTop ;   
+    
+    let scroollPosY = window.scrollY;
+    let tolerance = 100;   
+
+    if  (scroollPosY > ySection01 - tolerance){
+        currentPos = idSection01;
+    }
+    if  (scroollPosY > ySection02 - tolerance){
+        currentPos = idSection02;
+    }
+    if  (scroollPosY > ySection03 - tolerance){
+        currentPos = idSection03;
+    }
+    if  (scroollPosY > ySection04 - tolerance){
+        currentPos = idSection04;
+    }
+
+    console.clear();      
+    console.log(idSection01, ySection01);  
+    console.log(idSection02, ySection02);  
+    console.log(idSection03, ySection03);  
+    console.log(idSection04, ySection04);  
+    console.log(`Posicion de Y: ${scroollPosY}. tipo de dato: ${typeof scroollPosY}`);
+    console.log(`seccion actual : ${currentPos}`);
+
+
+    $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
+                 item.classList.remove('navbar-selected');
+                });  
+      
+    $d.getElementById(`${currentPos}_desktop_navbar_link`).classList.add('navbar-selected');    
+    
+    $d.querySelectorAll('.header__navbar-mobile__list__item>a').forEach((item) => {            
+        item.classList.remove('navbar-mobile-selected');
+       });  
+
+    $d.getElementById(`${currentPos}_mobile_navbar_link`).classList.add('navbar-mobile-selected');    
+     
+
+});
