9bc5d98d411bf2b4b030d96e18e5dee9182c260e
hernanruscica <cesarhernanruscica@gmail.com>
2023-09-08 22:11:03 -0300

    algunos ajustes de alturas y navbar y pagina de proyectos

--- scroll_sections.js
+++ scroll_sections.js
@@ -4,30 +4,33 @@ window.addEventListener('scroll', () => {
     let idSection02 = 'myskills';
     let idSection03 = 'recentprojects';
     let idSection04 = 'contactme';    
+    let idFooter = 'footer';
 
     let element01 = $d.getElementById(idSection01); 
     let element02 = $d.getElementById(idSection02); 
     let element03 = $d.getElementById(idSection03); 
     let element04 = $d.getElementById(idSection04);  
+    let footer = $d.getElementById(idFooter);  
 
     let ySection01 =  element01.offsetTop ; 
     let ySection02 =  element02.offsetTop ;
     let ySection03 =  element03.offsetTop ;
     let ySection04 =  element04.offsetTop ;   
+    let yFooter =  footer.offsetTop ;  
     
     let scroollPosY = window.scrollY;
-    let tolerance = 100;   
+    let tolerance = 200;   
 
-    if  (scroollPosY > ySection01 - tolerance){
+    if  (scroollPosY > ySection01 - 20){
         currentPos = idSection01;
     }
-    if  (scroollPosY > ySection02 - tolerance){
+    if  (scroollPosY > ySection02 - 20){
         currentPos = idSection02;
     }
-    if  (scroollPosY > ySection03 - tolerance){
+    if  (scroollPosY > ySection03 - 20){
         currentPos = idSection03;
     }
-    if  (scroollPosY > ySection04 - tolerance){
+    if  (scroollPosY > ySection04 - tolerance ){
         currentPos = idSection04;
     }
 
@@ -36,6 +39,7 @@ window.addEventListener('scroll', () => {
     console.log(idSection02, ySection02);  
     console.log(idSection03, ySection03);  
     console.log(idSection04, ySection04);  
+    console.log(idFooter, yFooter);  
     console.log(`Posicion de Y: ${scroollPosY}. tipo de dato: ${typeof scroollPosY}`);
     console.log(`seccion actual : ${currentPos}`);
 
