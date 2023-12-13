fe973ee5dc2714ad4f56b6db530741be6eaee1b4
hernanruscica <cesarhernanruscica@gmail.com>
2023-12-13 14:09:01 -0300

    terminado

--- scroll_sections.js
+++ scroll_sections.js
@@ -4,7 +4,7 @@
 let idSection01 = 'aboutme';
 let idSection02 = 'myskills';    
 let idSection03 = 'recentprojects';
-let idSection04 = 'links';
+let idSection04 = 'myLinks';
 let idSection05 = 'contactme';        
 let idFooter = 'footer';
 
@@ -19,6 +19,7 @@ let ySection02 =  null ;
 let ySection03 =  null ;
 let ySection04 =  null ;   
 let ySection05 =  null ;  
+let yFooter = null;
 
 let currentPos = 'aboutme'; 
 
@@ -29,44 +30,54 @@ document.addEventListener('DOMContentLoaded', () => {
     element03 = document.getElementById(idSection03); 
     element04 = document.getElementById(idSection04);  
     element05 = document.getElementById(idSection05);  
+    elementFooter = document.getElementById(idFooter);  
    
-    ySection01 =  0 ;     
-    ySection02 =  element01.offsetTop ;
-    ySection03 =  element02.offsetTop ;
-    ySection04 =  element03.offsetTop ;   
-    ySection05 =  element04.offsetTop ;   
-   
+    
+    
+    setTimeout(function() {
+        ySection01 = 0;
+        ySection02 = element02.offsetTop;
+        ySection03 = element03.offsetTop;
+        ySection04 = element04.offsetTop;
+        ySection05 = element05.offsetTop;
+        yFooter = elementFooter.offsetTop;  
+        
+        console.log(element04, ySection04);
+        
+    }, 500);
+
+    
 })
 
-window.addEventListener('scroll', () => {            
-    
+window.addEventListener('scroll', () => {               
     
     let tolerance = null;//   mobile
     if (window.innerWidth < 1000){
-        tolerance = 0;
+        tolerance = 30;
     }else {
-        tolerance = 120;
+        tolerance = 100;
     }   
 
-    let scroollPosY = window.scrollY;
+    let scroollPosY = window.scrollY + tolerance;
     
-console.log(ySection01, ySection02, ySection03, ySection04, scroollPosY, currentPos)
+    console.log(scroollPosY, ySection01, ySection02, ySection03, ySection04, ySection05, yFooter,  currentPos)
 
-    if  (scroollPosY > ySection01 ){
+    if  (scroollPosY > ySection01 && scroollPosY < ySection02 ){
         currentPos = idSection01;
     }
-    if  (scroollPosY > ySection02 ){
+    if  (scroollPosY > ySection02 && scroollPosY < ySection03){
         currentPos = idSection02;
     }
-    if  (scroollPosY > ySection03 ){
+    if  (scroollPosY > ySection03 && scroollPosY < ySection04){
         currentPos = idSection03;
     }
-    if  (scroollPosY > ySection04  ){
+    if  (scroollPosY > ySection04  && scroollPosY < ySection05){
         currentPos = idSection04;
     }
-    if  (scroollPosY > ySection05 - tolerance ){
+    if  (scroollPosY > ySection05  && scroollPosY < yFooter){
         currentPos = idSection05;
-    }   
+    }
+     
 
 console.log(currentPos)
      $d.querySelectorAll('.header__navbar-desktop__list__item>a').forEach((item) => {            
